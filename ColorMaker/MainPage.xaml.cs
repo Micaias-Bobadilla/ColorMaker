2902215381887128f1b6dcda8810dc39d1777cbf
Ab <mickyamb@gmail.com>
2022-11-30 07:11:17 -0300

    Agregado una pequeña firma

--- ColorMaker/MainPage.xaml.cs
+++ ColorMaker/MainPage.xaml.cs
@@ -2,7 +2,7 @@
 
 public partial class MainPage : ContentPage
 {
-	
+	bool isRandom;
 	public MainPage()
 	{
 		InitializeComponent();
@@ -10,22 +10,27 @@ public partial class MainPage : ContentPage
 
     private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
     {
-		var red = sldRed.Value;
-		var green = sldGreen.Value;
-		var blue = sldBlue.Value;
-		Color color = Color.FromRgb(red, green, blue);
+		if (!isRandom)
+        {
+            var red = sldRed.Value;
+            var green = sldGreen.Value;
+            var blue = sldBlue.Value;
+            Color color = Color.FromRgb(red, green, blue);
 
-		SetColor(color);
+            SetColor(color);
+
+        }
 
     }
 	private void SetColor( Color color) { 
-		btnRandom.BackgroundColor	= color;
+		btnRandom.BackgroundColor = color;
 		Container.BackgroundColor = color;
 		lblHex.Text = color.ToHex();
 	}
 
     private void btnRandom_Clicked(object sender, EventArgs e)
     {
+		isRandom= true;
 		var random = new Random();
 
 		var color = Color.FromRgb(
@@ -37,6 +42,7 @@ public partial class MainPage : ContentPage
 		sldRed.Value = color.Red;
 		sldGreen.Value = color.Green;
 		sldBlue.Value = color.Blue;
+		isRandom = false;
     }
 }
 
